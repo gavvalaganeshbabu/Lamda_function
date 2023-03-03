c99c0243a0b487100b2a5fef516cae3aa4a041d0
gavvalaganeshbabu <ganeshgavvala@gmail.com>
2023-03-03 07:54:20 +0530

    Master code Added

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lamda_function
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Validating data by Lamda Functions");
+        }
+    }
+}
